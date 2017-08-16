a7111df6ab2a7e7c0b959959c358f608af972584
chombatant <florian.hollandt@gmail.com>
2017-08-16 18:06:11 +0200

    170816: Moving coachybot to facebook

--- coachybot-facebook.py
+++ coachybot-facebook.py
@@ -25,7 +25,7 @@ def verify():
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
 
-return "Hello world", 200
+    return "Hello world", 200
 
 
 @app.route('/', methods=['POST'])
