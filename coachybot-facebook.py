add92a6514cb53657103035ea952292e1214414d
chombatant <florian.hollandt@gmail.com>
2017-08-23 07:42:56 +0200

    170823: Reset to before Telegram experiment

--- coachybot-facebook.py
+++ coachybot-facebook.py
@@ -360,6 +360,6 @@ def get_user_information( recipient_id):
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
-    print "Starting the app..." 
+    print "Starting the app... " 
     port = int( os.environ.get('PORT', 5000))
     app.run( host='0.0.0.0', port=port)
